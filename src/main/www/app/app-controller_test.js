e82061b48080c7bdf91b2acf87077953e27af62f
Gabor Cz <gabor_czigola@epam.com>
2014-04-17 12:51:54 +0800

    Tests added.

--- src/main/www/app/app-controller_test.js
+++ src/main/www/app/app-controller_test.js
@@ -1,7 +1,28 @@
-describe("earth controllers", function() {
+describe("earth controller messages", function() {
     beforeEach(module("earthApp")); 
     
-    it("some test", function() {
+    var $scope, deferred, sut, messages = ["somevalue"];
+    
+    beforeEach(inject(function($controller, $rootScope, $q) {
+        $scope = $rootScope.$new();
         
+        var messageService = jasmine.createSpyObj('CompanyService', ['getMessages']);
+        messageService.getMessages.andCallFake(function () {
+            deferred = $q.defer();
+            return deferred.promise;  
+        });
+        
+        sut = $controller('MessagesController', {
+            '$scope': $scope,
+            'messageService': messageService
+        });
+    }));
+    
+    it('should be in the scope', function() {
+        deferred.resolve(messages);
+        $scope.$digest();
+        
+        expect($scope.messages).toBe(messages);
+        expect($scope.count).toBe(messages.length);
     });
 });
